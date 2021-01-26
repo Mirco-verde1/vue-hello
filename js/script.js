f062c7908421db4a1dab135b3975b530ec49f98f
mirco verderosa <verderosamircowork@gmail.com>
2021-01-26 14:53:59 +0100

    Fine esercizio assegnato

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere un’immagine presa anch’essa da un data
+
+var app = new Vue({
+ el: '#msg-text',
+ data:{
+ message:'Ciao io sarei un H1!Sotto di me trovi....',
+ img:'https://www.dotcom-monitor.com/blog/wp-content/uploads/sites/3/2020/05/Vue-logo-1.png'
+ }
+});
+
+
+
+
+Vue.config.devtools = true;
